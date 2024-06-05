6f4c554cda76f4fc4e474f77fe78ea071ab8e197
sakshiipawar <pawarsakshi1805@gmail.com>
2024-06-05 13:59:15 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./MyComponents/Header";
+import Todos from "./MyComponents/Todos";
+import Footer from "./MyComponents/Footer";
+import React, { useState, useEffect } from "react";
+import AddTodo from "./MyComponents/AddTodo";
+import About from "./MyComponents/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+  };
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title, desc);
+    let srno;
+    if (todos.length === 0) {
+      srno = 1;
+    } else {
+      srno = todos[todos.length - 1].srno + 1;
+    }
+
+    const myTodo = {
+      srno: srno,
+      title: title,
+      desc: desc,
+    };
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header title="My Todos List" searchbar={true} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <AddTodo addTodo={addTodo} />
+                <Todos todos={todos} onDelete={onDelete} />
+              </>
+            }
+          />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/MyComponents/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <>
+        <p>
+          Todo Lists are the lists that we generally use to maintain our
+          day-to-day tasks or list of everything that we have to do, with the
+          most important tasks at the top of the list, and the least important
+          tasks at the bottom. In this article, we are going to learn how to
+          JavaScript Project on Todo List.
+        </p>
+      </>
+    </div>
+  );
+};
+
+export default About;
